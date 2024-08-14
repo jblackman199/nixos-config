dec9392f4eb4aac6456edc5b2738b4ca8cdc924d
jblackman199 <jblackman199@gmail.com>
2024-08-14 10:58:37 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,5 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# Edit this configuration file to define what should be installed on your system. Help is available
+# in the configuration.nix(5) man page and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, lib, pkgs, ... }:
 
@@ -22,7 +21,7 @@
     };
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest; # Install the latest kernel
+  # boot.kernelPackages = pkgs.linuxPackages_latest; # Install the latest kernel
 
   boot.binfmt.registrations.appimage = { # Make AppImages work
     wrapInterpreterInShell = false;
@@ -33,16 +32,7 @@
     magicOrExtension = ''\x7fELF....AI\x02'';
   };
 
-  swapDevices = [ {
-    device = "/var/lib/swapfile";
-    size = 8192;
-  } ];
-
   # Hardware stuff
-
-  hardware.opengl = { # Enable OpenGL
-    enable = true;
-  };
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
@@ -54,36 +44,35 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-    };
+    #jack.enable = true;  If you want to use JACK applications, uncomment this
+  };
+
+  hardware.opengl = { # Enable OpenGL
+    enable = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"]; # Load nvidia driver for Xorg and Wayland
 
   hardware.nvidia = {
 
     modesetting.enable = true;  # Modesetting is required
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = true;
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail. Enable this if
+    # you have graphical corruption issues or application crashes after waking up from sleep.
+    # This fixes it by saving the entire VRAM memory to /tmp/ instead of just the bare essentials.
+    powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    # Use the NVidia open source kernel module (not to be confused with the independent third-party
+    # "nouveau" open source driver). Support is limited to the Turing and later architectures. Full
+    # list of supported GPUs is at: https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+ Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
 
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`
-    nvidiaSettings = false;
+    nvidiaSettings = false; # Enable the Nvidia settings menu, accessible via `nvidia-settings`
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
@@ -94,8 +83,7 @@
   networking.hostName = "jaydens-nix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.default = "http://user:password@proxy:port/";  # Configure network proxy if necessary
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   networking.networkmanager.enable = true;  # Enable networking
@@ -116,9 +104,7 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  # media-session.enable = true;  # use the example session manager
 
   users.users.jblackman199 = {  # Define a user account. Don't forget to set a password with ‘passwd’
     isNormalUser = true;
@@ -129,23 +115,22 @@
   nix.extraOptions =  # Enable nix experimental features
   "experimental-features = nix-command flakes";
 
-  nixpkgs.config.allowUnfree = true;  # Allow unfree packages
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
+  # This value determines the NixOS release from which the default settings for stateful data,
+  # like file locations and database versions on your system were taken. It‘s perfectly fine
+  # and recommended to leave this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
-  system.autoUpgrade.enable = true; # Automatically upgrade
-  system.autoUpgrade.allowReboot = true;
+  system.autoUpgrade = {
+    enable = true; # Automatically upgrade
+    randomizedDelaySec = "30sec"; # Raise upgrade attempts
+  };
 
   nix.gc = { # Collect garbage weekly
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 14d";
+    options = "--delete-older-than 5d";
   };
 
   nix.optimise.automatic = true; # Automatically optimize /nix/store weekly
@@ -153,30 +138,15 @@
 
   # List services that you want to enable:
 
-  services.xserver.videoDrivers = ["nvidia"]; # Load nvidia driver for Xorg and Wayland
-
   services.snap.enable = true;  # Enable snap
   services.flatpak.enable = true; # Enable flatpak
-
-  programs.steam = {  # Install and enable Steam to run
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ # Let nonfree Steam dependencies work
-    "steam"
-    "steam-original"
-    "steam-run"
-  ];
 
   services.xserver.enable = false;  # Enable the X11 windowing system. You can disable this if you're only using the Wayland session.
 
   services.displayManager.sddm.enable = true; # Enable SDDM
   services.desktopManager.plasma6.enable = true;  # Enable Plasma 6
   services.displayManager.defaultSession = "plasma"; # Set default session plasme = wayland, plasmax11 = x11
-  services.displayManager.sddm.wayland.enable = true; # Enable Plasma Wayland session
+  services.displayManager.sddm.wayland.enable = true; # Set SDDM to Wayland
   services.displayManager.sddm.autoNumlock = true;  # Enable numlock on SDDM
   environment.plasma6.excludePackages = with pkgs.kdePackages; [  # Exclude listed packages
     oxygen
@@ -193,17 +163,27 @@
 
   services.printing.enable = true;  # Enable CUPS to print documents
 
+  services.printing.drivers = with pkgs; [ # List printer drivers to install
+    cnijfilter2
+  ];
+
   services.power-profiles-daemon.enable = false; # Disable power profiles daemon
   services.tlp.enable = true; # Enable tlp
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.tailscale.enable = true; # Install and enable tailscale
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  virtualisation.libvirtd.enable = true; # Install and enable libvirt
+  programs.virt-manager.enable = true; # Install and enable virt-manager
+
+  # services.openssh.enable = true; # Enable the OpenSSH daemon.
+
+  # services.xserver.libinput.enable = true;  # Enable touchpad support (enabled default in most desktopManager).
+
+  nixpkgs.config.allowUnfree = true;  # Allow unfree packages
 
   environment.systemPackages = with pkgs; [ # List packages installed in system profile. To search, run: nix search wget
-    birdtray
+    bottom
+    cpufetch
     discord
     fastfetch
     fish
@@ -212,33 +192,41 @@
     hunspell
     hunspellDicts.en_US
     hyphen
-    jre8
     jupyter
     kdePackages.discover
+    kdePackages.dolphin-plugins
+    kdePackages.ffmpegthumbs
+    kdePackages.isoimagewriter
+    kdePackages.kasts
     kdePackages.kate
     kdePackages.kdeconnect-kde
+    kdePackages.kdegraphics-thumbnailers
+    kdePackages.kde-gtk-config
+    kdePackages.kimageformats
     kdePackages.partitionmanager
     kdePackages.plasma-browser-integration
     kdePackages.print-manager
-    kdePackages.wrapQtAppsHook
+    kdePackages.qtimageformats
+    kdePackages.sddm-kcm
+    kdePackages.sonnet
+    kdePackages.svgpart
     libreoffice-qt6-fresh
     mythes
-    neovim
     ollama
     onedrive
     onedrivegui
-    python312Packages.scipy
-    python312Packages.sympy
+    open-webui
     qalculate-gtk
     qbittorrent
+    ramfetch
     rnote
     rssguard
     sage
     signal-desktop
     superTuxKart
-    thunderbird
+    thunderbird-128
     tlp
-    ventoy-full
+    ventoy
     vlc
     xdg-desktop-portal-gtk
     xournalpp
@@ -252,6 +240,20 @@
     noto-fonts-color-emoji
     liberation_ttf
     libertine
+  ];
+
+  programs.steam = {  # Install and enable Steam to run
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ # Let nonfree Steam dependencies work
+    "steam"
+    "steam-original"
+    "steam-run"
+    "teams"
   ];
 
   networking.firewall = { # Open ports for KDE Connect
@@ -269,9 +271,7 @@
 
   programs.dconf.enable = true; # Fix GTK themes
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
+  # programs.mtr.enable = true; # Some programs need SUID wrappers, can be configured further or are started in user sessions.
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
